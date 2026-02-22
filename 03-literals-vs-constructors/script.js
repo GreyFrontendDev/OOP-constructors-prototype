5926a442f34f165515d6d2115a4e2fef6d9093f9
GreyFrontendDev <greyfrontenddev@gmail.com>
2026-02-22 00:46:56 +0000

    Intro into OOP, principles, obj literals and this keyword, and difference between literals and built-in constructors

---
+++ 03-literals-vs-constructors/script.js
@@ -0,0 +1,60 @@
+const strLit = 'Hello';
+const strObj = new String('Hello');
+
+console.log(strLit, typeof strLit);
+console.log(strObj, typeof strObj);
+
+// Boxing
+console.log(strLit.toUpperCase());
+console.log(strLit[0]);
+
+// Unboxing
+console.log(strObj.valueOf(), typeof strObj.valueOf());
+
+console.log(strLit.constructor);
+console.log(strObj.constructor);
+
+console.log(strLit instanceof String); // false as it was not created from string
+console.log(strObj instanceof String); // True as it was gotten from the new String()
+
+// Other Types
+
+// Number
+const numLit = 20;
+const numObj = new Number(20);
+
+console.log(numLit, typeof numLit);
+console.log(numObj, typeof numObj);
+
+// Boolean
+const boolLit = true;
+const boolObj = new Boolean(true);
+
+console.log(boolLit, typeof boolLit);
+console.log(boolObj, typeof boolObj);
+
+// Array
+const arrLit = [1, 2, 3, 4, 5];
+const arrObj = new Array(1, 2, 3, 4, 5);
+
+console.log(arrLit, typeof arrLit);
+console.log(arrObj, typeof arrObj);
+
+const funcLit = function (x) {
+  return x * x;
+};
+
+console.log(funcLit, typeof funcLit);
+
+console.log(funcLit(5));
+
+const funcObj = new Function('x', 'return x * x');
+
+console.log(funcObj(3));
+
+const obj1 = {};
+
+const obj2 = new Object();
+
+console.log(obj1, typeof obj1);
+console.log(obj2, typeof obj2);
